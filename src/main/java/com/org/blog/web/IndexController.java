c30e09c3ea6aaf5eb1e6576ac1538381ce3bb35e
Tina Huang <t42huang@gmail.com>
2020-07-20 13:03:00 -0400

    log

--- src/main/java/com/org/blog/web/IndexController.java
+++ src/main/java/com/org/blog/web/IndexController.java
@@ -3,14 +3,15 @@ package com.org.blog.web;
 import javassist.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index(){
-        int i = 9/0;
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id, @PathVariable String name){
+//        int i = 9/0;
+        System.out.println("------index------");
         return "index";
     }
     @GetMapping("/test")
