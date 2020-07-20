22b1fd177c593ab9c998dd00c20fa5382a1c47a8
Tina Huang <t42huang@gmail.com>
2020-07-20 13:31:14 -0400

    implement front code

--- src/main/java/com/org/blog/web/IndexController.java
+++ src/main/java/com/org/blog/web/IndexController.java
@@ -8,22 +8,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class IndexController {
 
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable Integer id, @PathVariable String name){
-//        int i = 9/0;
-        System.out.println("------index------");
-        return "index";
+    @GetMapping("/")
+    public String index(){
+
+        return "aboutme";
     }
-    @GetMapping("/test")
-    public String test(){
-        String blog = null;
-        if(blog == null){
-            try {
-                throw new NotFoundException("Blog is not exist");
-            } catch (NotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-        return "index";
-    }
+//
+//    @GetMapping("/{id}/{name}")
+//    public String index(@PathVariable Integer id, @PathVariable String name){
+////        int i = 9/0;
+//        System.out.println("------test------");
+//        return "test";
+//    }
+//    @GetMapping("/test")
+//    public String test(){
+//        String blog = null;
+//        if(blog == null){
+//            try {
+//                throw new NotFoundException("Blog is not exist");
+//            } catch (NotFoundException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        return "test";
+//    }
 }
