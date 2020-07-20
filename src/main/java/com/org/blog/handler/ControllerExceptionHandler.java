816a50603c81698268b6a0a534e30e4664c514d3
Tina Huang <t42huang@gmail.com>
2020-07-20 11:34:34 -0400

    leave exception with status handled by spring

--- src/main/java/com/org/blog/handler/ControllerExceptionHandler.java
+++ src/main/java/com/org/blog/handler/ControllerExceptionHandler.java
@@ -2,11 +2,14 @@ package com.org.blog.handler;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.PreparedStatement;
 
 //拦截所有标注有controller的注解器
 @ControllerAdvice
@@ -17,6 +20,14 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ModelAndView exceptionHandler(HttpServletRequest request, Exception e){
         logger.error("Request URL {}, Exception: {}", request.getRequestURL(), e);
+
+        if(AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null){
+            try {
+                throw e;
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        }
 
         ModelAndView mv = new ModelAndView();
         mv.addObject("url", request.getRequestURL());
--- src/main/java/com/org/blog/web/IndexController.java
+++ src/main/java/com/org/blog/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.org.blog.web;
 
+import javassist.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,18 @@ public class IndexController {
     @GetMapping("/")
     public String index(){
         int i = 9/0;
+        return "index";
+    }
+    @GetMapping("/test")
+    public String test(){
+        String blog = null;
+        if(blog == null){
+            try {
+                throw new NotFoundException("Blog is not exist");
+            } catch (NotFoundException e) {
+                e.printStackTrace();
+            }
+        }
         return "index";
     }
 }
